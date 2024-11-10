6a157ecb832fcb3b730fe8738bd121eed80d9083
Abdelrahman Khalifa <87104141+AbdoAhmed-Khalifa@users.noreply.github.com>
2024-11-10 15:42:03 +0200

    first commmit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          65: '#FF8D4C',
+          70: '#FF9E66',
+          75: '#FFAE80',
+          80: '#FFBE99',
+          90: '#FFDECC',
+          95: '#FFEFE5',
+          97: '#FFF5F0',
+          99: '#FFFCFA',
+        },
+        dark: {
+          10: '#191919',
+          15: '#262626',
+          20: '#333333',
+          30: '#4C4C4D',
+          35: '#59595A',
+          40: '#656567',
+          60: '#98989A',
+          70: '#B3B3B3',
+        },
       },
     },
   },
