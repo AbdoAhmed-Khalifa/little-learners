63c07a9bc79d001b81d84f6f8f19627f224be077
Abdelrahman Khalifa <87104141+AbdoAhmed-Khalifa@users.noreply.github.com>
2024-11-14 17:42:57 +0200

    finsih page card section

---
+++ app/_components/pageCardComponents/CardButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CardButton() {
+  return (
+    <div className="pt-6">
+      <button className="px-6 py-3 bg-primary-90 text-gray-800 font-semibold border-2 rounded-lg border-dark-15 shadow-button hover:bg-primary-65 flex items-center justify-center w-full transition duration-300 ease-in-out ">
+        Learn More
+        <span className="ml-2 text-xl">→</span>
+      </button>
+      </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import PageCardsSection from "./_components/pageCardComponents/PageCardsSection";
+
 export default function Home() {
-  return <div>Home</div>;
+  return <div>Home
+
+    <PageCardsSection/>
+  </div>;
 }
