69e0a69a8a39e0c66adb6e826c4f4669a32fa8e1
Abdelrahman Khalifa <87104141+AbdoAhmed-Khalifa@users.noreply.github.com>
2024-11-17 21:40:07 +0200

    design page header section

---
+++ app/_components/PageHeader.tsx
@@ -0,0 +1,115 @@
+export default function PageHeader({
+  page,
+  title,
+  content,
+}: {
+  title: string;
+  content: string;
+  page: string;
+}) {
+  return (
+    <div className="mx-5 mt-10 rounded-xl border-2 border-dark-15 bg-white shadow-card md:mx-10 lg:mx-20">
+      <div className="relative flex items-center justify-center pb-10 pt-24">
+        <div className="absolute left-0 top-0">
+          <svg
+            width="130"
+            height="130"
+            viewBox="0 0 200 200"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_123_2429)">
+              <path
+                d="M0 24.9957L24.9957 0H49.9914V24.9957L24.9957 49.9914H0V24.9957Z"
+                fill="#FFBE99"
+              />
+              <path d="M49.9918 0H24.9961V24.9957H49.9918V0Z" fill="#262626" />
+              <path
+                d="M0 75.0043L24.9957 49.9915H49.9914V75.0043L24.9957 100H0V75.0043Z"
+                fill="#FFBE99"
+              />
+              <path
+                d="M49.9918 49.9915H24.9961V74.9872H49.9918V49.9915Z"
+                fill="#262626"
+              />
+              <path
+                d="M0 124.996L24.9957 100H49.9914V124.996L24.9957 149.991H0V124.996Z"
+                fill="#FFBE99"
+              />
+              <path
+                d="M49.9918 100H24.9961V124.996H49.9918V100Z"
+                fill="#262626"
+              />
+              <path
+                d="M0 174.987L24.9957 149.991H49.9914V174.987L24.9957 200H0V174.987Z"
+                fill="#FFBE99"
+              />
+              <path
+                d="M49.9918 149.991H24.9961V174.987H49.9918V149.991Z"
+                fill="#262626"
+              />
+              <path
+                d="M49.9912 24.9957L75.0041 0H99.9998V24.9957L75.0041 49.9914H49.9912V24.9957Z"
+                fill="#FFBE99"
+              />
+              <path d="M99.9996 0H75.0039V24.9957H99.9996V0Z" fill="#262626" />
+              <path
+                d="M49.9912 75.0043L75.0041 49.9915H99.9998V75.0043L75.0041 100H49.9912V75.0043Z"
+                fill="#FFBE99"
+              />
+              <path
+                d="M99.9996 49.9915H75.0039V74.9872H99.9996V49.9915Z"
+                fill="#262626"
+              />
+              <path
+                d="M49.9912 124.996L75.0041 100H99.9998V124.996L75.0041 149.991H49.9912V124.996Z"
+                fill="#FFBE99"
+              />
+              <path
+                d="M99.9996 100H75.0039V124.996H99.9996V100Z"
+                fill="#262626"
+              />
+              <path
+                d="M100 24.9957L124.996 0H149.991V24.9957L124.996 49.9914H100V24.9957Z"
+                fill="#FFBE99"
+              />
+              <path d="M149.992 0H124.996V24.9957H149.992V0Z" fill="#262626" />
+              <path
+                d="M149.991 24.9957L174.987 0H200V24.9957L174.987 49.9914H149.991V24.9957Z"
+                fill="#FFBE99"
+              />
+              <path d="M199.983 0H174.987V24.9957H199.983V0Z" fill="#262626" />
+              <path
+                d="M100 75.0043L124.996 49.9915H149.991V75.0043L124.996 100H100V75.0043Z"
+                fill="#FFBE99"
+              />
+              <path
+                d="M149.992 49.9915H124.996V74.9872H149.992V49.9915Z"
+                fill="#262626"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_123_2429">
+                <rect width="200" height="200" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+
+        <div className="mx-12 grid grid-cols-1 items-center justify-center justify-items-center gap-10 text-left md:grid-cols-2 lg:mx-20">
+          <div className="flex flex-col items-center justify-center md:items-start">
+            <button className="mb-4 rounded-md border-2 border-dark-15 px-3 py-1.5 font-outfit text-sm font-medium text-dark-20">
+              {page}
+            </button>
+            <h1 className="font-raleway text-4xl font-bold text-dark-10">
+              {title}
+            </h1>
+          </div>
+          <p className="mt-4 max-w-xl font-outfit font-medium text-dark-20">
+            {content}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import MissionVisionsSection from "../_components/Mission&VisionsComponents/MissionVisionsSection";
+import PageHeader from "../_components/PageHeader";
 
 export default function About() {
   return (
     <>
+      <PageHeader
+        page="Overview"
+        title="Welcome to Little Learners Academy"
+        content="A leading kinder garden school dedicated to providing a nurturing and stimulating environment for young learners. With a commitment to excellence in early education, we believe in shaping curious minds and building a strong foundation for a lifelong love of learning. Our holistic approach fosters intellectual, social, emotional, and physical development, ensuring that each child reaches their full potential."
+      />
       <MissionVisionsSection />
     </>
   );
