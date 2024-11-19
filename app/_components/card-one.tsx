eaa1f798d03a1efdee9b4d60f09b9eb3a145d848
Mahmoud Abbas <mahmoudabbas393@gmail.com>
2024-11-19 03:47:54 +0200

    feat:create Awards Section

--- app/_components/card-one.tsx
+++ app/_components/card-one.tsx
@@ -10,7 +10,7 @@ export default function CardOne({
   icon: ReactNode;
 }) {
   return (
-    <section className="relative rounded-xl border-2 border-dark-15 pb-[30px] pl-[30px] pr-10 pt-[50px] shadow-[6px_6px_0_2px_rgpa(30,30,30,1)] shadow-[#1E1E1E] md:px-[50px] md:pb-[50px] md:pt-20">
+    <section className="relative h-full rounded-xl border-2 border-dark-15 pb-[30px] pl-[30px] pr-10 pt-[50px] shadow-[6px_6px_0_2px_rgpa(30,30,30,1)] shadow-[#1E1E1E] md:px-[50px] md:pb-[50px] md:pt-20">
       <span className="absolute -top-7 left-[24px] inline-block rounded-lg border-2 border-dark-15 bg-primary-80 p-4 md:-top-[35px] md:left-[30px] md:rounded-xl md:p-5">
         {icon}
       </span>
