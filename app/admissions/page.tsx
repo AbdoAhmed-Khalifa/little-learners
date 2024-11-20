da4d90f68347c364ff7b7bda4038c2ca461b4835
Mahmoud Abbas <mahmoudabbas393@gmail.com>
2024-11-20 04:23:53 +0200

    feat: add the page header component to the addmission section

--- app/admissions/page.tsx
+++ app/admissions/page.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React from "react";
+import PageHeader from "../_components/PageHeader";
 
 export default function Admissions() {
-  return <div>Admissions</div>;
+  return (
+    <>
+      <PageHeader
+        title="Join Our Family of Young Learners"
+        content="At Little Learners Academy, we welcome you to embark on an exciting educational journey for your child. Our admission process is designed to be transparent, straightforward, and inclusive. Here's a step-by-step guide to joining our school "
+        page="Admission"
+      />
+    </>
+  );
 }
